b75b7b17c19be8221e4c3e225c03785a10316784
Kevin Lam <find.keviin@gmail.com>
2020-02-19 15:59:48 -0500

    [FEAT]Created a popup component.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Link, Route, Switch } from "react-router-dom";
 
+import Home from "./component/home";
+import Intro from "./component/introduction";
+import Work from "./component/work";
+import About from "./component/about";
+import Contact from "./component/contact";
+import ErrorPage from "./component/error_page";
+import Popup from "./component/popup";
 
-import Home from './component/home';
-import Intro from './component/introduction';
-import Work from './component/work';
-import About from './component/about';
-import Contact from './component/contact';
-import ErrorPage from './component/error_page';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showPopup: false };
+  }
 
-const App = () => {
-  return (
-    <div className="App">
-      {/* <header className="App-header">
+  togglePopup() {
+    this.setState({
+      showPopup: !this.state.showPopup
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1> Simple Popup Example In React Application </h1>
+        <button onClick={this.togglePopup.bind(this)}>
+          {" "}
+          Click To Launch Popup
+        </button>
+
+        {this.state.showPopup ? (
+          <Popup
+            text='Click "Close Button" to hide popup'
+            closePopup={this.togglePopup.bind(this)}
+          />
+        ) : null}
+        {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Home/>
         <Intro/>
@@ -22,41 +46,50 @@ const App = () => {
         <About/>
         <Contact/>
       </header> */}
-      <div id="wrapper">
-        <header id="header">
-          <div className="logo">
+        <div id="wrapper">
+          <header id="header">
             <div className="logo">
-            <img src={logo} className="logo" alt="logo" />
+              <div className="logo">
+                <img src={logo} className="logo" alt="logo" />
+              </div>
             </div>
-          </div>
-          <div className="content">
-            <div className="inner">
-              <Home/>
+            <div className="content">
+              <div className="inner">
+                <Home />
+              </div>
             </div>
-          </div>
-          <nav>
-            <ul>
-              <li><a href="/intro">Intro</a></li>
-              <li><a href="/work">Work</a></li>
-              <li><a href="/about">About</a></li>
-              <li><a href="/contact">Contact</a></li>
-              {/*<li><a href="#elements">Elements</a></li>*/}
-            </ul>
-            <Switch>
-              <Route path="/intro" exact component={Intro} />
-              <Route path="/work" exact component={Work} />
-              <Route path="/about" exact component={About} />
-              <Route path="/contact" exact component={Contact} />
-              <Route path="*" component={ErrorPage} />
-            </Switch>
-          </nav>
-        </header>
-        <footer id="footer">
-        <p className="copyright">KevinLam.dev</p>
-      </footer>
+            <nav>
+              <ul>
+                <li>
+                  <a href="/intro">Intro</a>
+                </li>
+                <li>
+                  <a href="/work">Work</a>
+                </li>
+                <li>
+                  <a href="/about">About</a>
+                </li>
+                <li>
+                  <a href="/contact">Contact</a>
+                </li>
+                {/*<li><a href="#elements">Elements</a></li>*/}
+              </ul>
+              <Switch>
+                <Route path="/intro" exact component={Intro} />
+                <Route path="/work" exact component={Work} />
+                <Route path="/about" exact component={About} />
+                <Route path="/contact" exact component={Contact} />
+                {/* <Route path="*" component={ErrorPage} /> */}
+              </Switch>
+            </nav>
+          </header>
+          <footer id="footer">
+            <p className="copyright">KevinLam.dev</p>
+          </footer>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
