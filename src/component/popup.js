2be72b7e43314c0b35e6fb1d839598bf1aadbe5e
Kevin Lam <find.keviin@gmail.com>
2020-03-17 17:45:27 -0400

    [FEAT] Successfully render all components with popup.js

--- src/component/popup.js
+++ src/component/popup.js
@@ -1,28 +1,44 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "../index.css";
 
 import Intro from "./introduction";
 import Work from "./work";
 import About from "./about";
 import Contact from "./contact";
-// import ErrorPage from "./error_page";
+import ErrorPage from "./error_page";
 
 class Popup extends Component {
   selectedComponent = {
-    Intro : Intro,
-    Work : Work,
-    About : About,
-    Contact : Contact,
-  }
+    Intro: Intro,
+    Work: Work,
+    About: About,
+    Contact: Contact
+  };
   render() {
+    const renderComponent = () =>
+    Object.keys(this.selectedComponent).map(function(key, index) {
+      let component = '';
+        //return the component that matches the key.
+        console.log("key in map", key);
+        console.log("idx in map", index);
+        if (key === "Intro") {
+          component = <Intro />;
+        } else if (key === "Work") {
+          component = <Work />;
+        } else if (key === "About") {
+          component = <About />;
+        } else if (key === "Contact") {
+          component = <Contact />;
+        } else {
+          component = <ErrorPage/>
+        }
+        return component;
+      });
+
     return (
       <div className="popup">
         <div className="popup_inner">
-          {/* <h1>{this.props.component}</h1> */}
-          {Object.keys(this.selectedComponent).map(function(key, index){
-            //return the component that matches the key.
-          return <Intro/>;
-          })}
+          { renderComponent() }
           <button onClick={this.props.closePopup}>CLOSE</button>
         </div>
       </div>
