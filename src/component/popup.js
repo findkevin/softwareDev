12901a8ecdd3d89db48fbc32716ae2e7722df7ad
Kevin Lam <find.keviin@gmail.com>
2020-03-18 22:58:03 -0400

    [FEAT] Successfully render dynamic component in Popup.js

--- src/component/popup.js
+++ src/component/popup.js
@@ -14,36 +14,13 @@ class Popup extends Component {
     About,
     Contact,
   };
-  render() {
-    let propsComponent = this.props.component;
-    console.log('propsComponent outside MAP:', propsComponent)
-    const renderComponent = () =>
-      Object.keys(this.avaliableComponents).map(function(key, index) {
-        let component = "";
-        //return the component that matches the key.
-        if (key === propsComponent) {
-          component = <Intro />;
-        } else if (key === propsComponent) {
-          component = <Work />;
-        } else if (key === propsComponent) {
-          component = <About />;
-        } else if (key === propsComponent) {
-          component = <Contact />;
-        }
-        console.log('--------------------Inside MAP----------------------')
-        console.log("key in map:", key);
-        console.log("idx in map:", index);
-        console.log("this.props.comp:", propsComponent);
-        console.log('if key = propsComp', key === propsComponent)
-        console.log('component:', component)
-        console.log('--------------------Before Return------------------')
-        return component;
-      });
 
+  render() {
+    const RenderComponent = this.avaliableComponents[this.props.component]
     return (
       <div className="popup">
         <div className="popup_inner">
-          {renderComponent()}
+          <RenderComponent />
           <button onClick={this.props.closePopup}>CLOSE</button>
         </div>
       </div>
